8761158103152b5c0f14c5943c9fa2642baee735
Alif Munim <alif.munim@ryerson.ca>
2021-03-27 23:57:35 -0400

    Implement summaries with sliding windows

--- src/main/java/com/flinklearn/realtime/windowing/WindowingOperations.java
+++ src/main/java/com/flinklearn/realtime/windowing/WindowingOperations.java
@@ -1,5 +1,7 @@
 package com.flinklearn.realtime.windowing;
 
+import com.flinklearn.realtime.datastreamapi.AuditTrail;
+
 import com.flinklearn.realtime.common.Utils;
 import com.flinklearn.realtime.datasource.KafkaStreamDataGenerator;
 import com.flinklearn.realtime.datastreamapi.AuditTrail;
@@ -9,6 +11,10 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 
 import org.apache.flink.streaming.api.datastream.DataStream;
+
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.api.common.typeinfo.Types;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -23,7 +29,7 @@ public class WindowingOperations {
             /*******************************************************************
              * Set up Flink environment
              *******************************************************************/
-
+            // Create stream execution environment
             final StreamExecutionEnvironment streamEnv =
                     StreamExecutionEnvironment.getExecutionEnvironment();
 
@@ -56,6 +62,54 @@ public class WindowingOperations {
             });
 
             /*******************************************************************
+             * Use sliding windows
+             *******************************************************************/
+            // Sliding window interval of 10 seconds, sliding by 5 seconds
+            DataStream<Tuple4<String, Integer, Long, Long>> slidingSummary =
+                    auditObj.map(i -> new Tuple4<String, Integer, Long, Long>(
+                            String.valueOf(System.currentTimeMillis()),
+                            1,
+                            i.getTimestamp(),
+                            i.getTimestamp()
+                    ))
+                    .returns(Types.TUPLE(
+                            Types.STRING,
+                            Types.INT,
+                            Types.LONG,
+                            Types.LONG
+                    ))
+                    .timeWindowAll(
+                            Time.seconds(10),
+                            Time.seconds(5)
+                    )
+                    .reduce((x, y) -> new Tuple4<String, Integer, Long, Long>(
+                            x.f0,
+                            x.f1 + y.f1,
+                            Math.min(x.f2, y.f2),
+                            Math.max(x.f3, y.f3)
+                    ));
+
+            // Pretty print the tuples
+            slidingSummary.map(new MapFunction<Tuple4<String, Integer, Long, Long>, Object>() {
+
+                @Override
+                public Object map(Tuple4<String, Integer, Long, Long> slidingSummary) throws Exception {
+                    SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+                    String minTime = format.format(new Date(Long.valueOf(slidingSummary.f2)));
+                    String maxTime = format.format(new Date(Long.valueOf(slidingSummary.f3)));
+                    System.out.println(
+                            "Sliding Summary : "
+                            + (new Date()).toString()
+                            + " Start Time : " + minTime
+                            + " End Time : " + maxTime
+                            + " Count : " + slidingSummary.f1
+                    );
+                    return null;
+                }
+            });
+
+
+            /*******************************************************************
              * Set up data source and execute
              *******************************************************************/
             //Start the Kafka Stream generator on a separate thread
@@ -65,7 +119,6 @@ public class WindowingOperations {
 
             // execute the streaming pipeline
             streamEnv.execute("Flink Kafka consumer and windowing");
-
 
         } catch(Exception e) {
             e.printStackTrace();
