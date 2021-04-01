4a7fe16e339ed7a629b626f521a4c9a6a3d074ae
Alif Munim <alif.munim@ryerson.ca>
2021-04-01 17:41:41 -0400

    Add missing line for current watermark

--- src/main/java/com/flinklearn/realtime/timeprocessing/EventTimeOperations.java
+++ src/main/java/com/flinklearn/realtime/timeprocessing/EventTimeOperations.java
@@ -31,157 +31,158 @@ public class EventTimeOperations {
 
     public static void main(String[] args) throws Exception {
 
-        /*******************************************************************
-         * Set up stream execution environment
-         *******************************************************************/
-        final StreamExecutionEnvironment streamEnv =
-                StreamExecutionEnvironment.getExecutionEnvironment();
-        streamEnv.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-        streamEnv.setParallelism(1);
+        try {
+            /*******************************************************************
+             * Set up stream execution environment
+             *******************************************************************/
+            final StreamExecutionEnvironment streamEnv =
+                    StreamExecutionEnvironment.getExecutionEnvironment();
+            streamEnv.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+            streamEnv.setParallelism(1);
 
-        /*******************************************************************
-         * Set up basic pipeline
-         *******************************************************************/
+            /*******************************************************************
+             * Set up basic pipeline
+             *******************************************************************/
+            String dataDir = "data/raw_audit_trail";
+            TextInputFormat auditFormat = new TextInputFormat(
+                    new Path(dataDir)
+            );
+            DataStream<String> auditString = streamEnv.readFile(
+                    auditFormat,
+                    dataDir,
+                    FileProcessingMode.PROCESS_CONTINUOUSLY,
+                    1000
+            );
 
-        String dataDir = "data/raw_audit_trail";
-        TextInputFormat auditFormat = new TextInputFormat(
-                new Path(dataDir)
-        );
-        DataStream<String> auditString = streamEnv.readFile(
-                auditFormat,
-                dataDir,
-                FileProcessingMode.PROCESS_CONTINUOUSLY,
-                1000
-        );
-
-        DataStream<AuditTrail> auditObject = auditString.map(
-                new MapFunction<String, AuditTrail>() {
-                    @Override
-                    public AuditTrail map(String atStr) throws Exception {
-                        System.out.println("* Received record: " + atStr);
-                        return new AuditTrail(atStr);
-                    }
-                }
-        );
-
-        /*******************************************************************
-         * Set up event time and watermarked data stream
-         *******************************************************************/
-
-        DataStream<AuditTrail> auditMarked = auditObject.assignTimestampsAndWatermarks(
-                new AssignerWithPunctuatedWatermarks<AuditTrail>() {
-
-                    transient long currentWatermark = 0L;
-                    int delay = 10000;
-                    int buffer = 2000;
-
-                    @Nullable
-                    @Override
-                    public Watermark checkAndGetNextWatermark(AuditTrail auditTrail, long nextTimestamp) {
-
-                        long currentTime = System.currentTimeMillis();
-
-                        if(currentWatermark == 0L) {
-
-                        } else if(currentTime - currentWatermark > delay) {
-                            currentWatermark = currentTime;
+            DataStream<AuditTrail> auditObject = auditString.map(
+                    new MapFunction<String, AuditTrail>() {
+                        @Override
+                        public AuditTrail map(String atStr) throws Exception {
+                            System.out.println("* Received record: " + atStr);
+                            return new AuditTrail(atStr);
                         }
-
-                        return new Watermark(currentWatermark - buffer);
-
                     }
+            );
 
-                    @Override
-                    public long extractTimestamp(AuditTrail auditTrail, long previousTimestamp) {
-                        return auditTrail.getTimestamp();
-                    }
-                }
-        );
+            /*******************************************************************
+             * Set up event time and watermarked data stream
+             *******************************************************************/
+            DataStream<AuditTrail> auditMarked = auditObject.assignTimestampsAndWatermarks(
+                    new AssignerWithPunctuatedWatermarks<AuditTrail>() {
 
-        /*******************************************************************
-         * Process watermarked stream
-         *******************************************************************/
+                        transient long currentWatermark = 0L;
+                        int delay = 10000;
+                        int buffer = 2000;
 
-        // Create output tag for late records
-        final OutputTag<Tuple2<String, Integer>> lateAuditTrail =
-                new OutputTag<Tuple2<String, Integer>>("late-audit-trail"){};
+                        @Nullable
+                        @Override
+                        public Watermark checkAndGetNextWatermark(AuditTrail auditTrail, long nextTimestamp) {
 
-        // Use single output stream operator to produce side output
-        SingleOutputStreamOperator<Tuple2<String, Integer>> finalTrail =
-                auditMarked
-                .map(i -> new Tuple2<String, Integer>
-                        (String.valueOf(i.getTimestamp()), 1)
-                )
-                .returns(Types.TUPLE(Types.STRING, Types.INT))
-                .timeWindowAll(Time.seconds(1))
-                .sideOutputLateData(lateAuditTrail)
-                .reduce((x, y) ->
-                        (new Tuple2<String, Integer>(x.f0, x.f1 + y.f1))
-                )
-                .map(
-                        new MapFunction<Tuple2<String, Integer>, Tuple2<String, Integer>>() {
-                            @Override
-                            public Tuple2<String, Integer> map(Tuple2<String, Integer> minuteSummary) throws Exception {
-                                String currentTime = (new Date()).toString();
-                                String eventTime =
-                                        (new Date(Long.valueOf(minuteSummary.f0))).toString();
+                            long currentTime = System.currentTimeMillis();
 
-                                System.out.println("Summary:"
-                                        + " Current Time: " + currentTime
-                                        + " Event Time: " + eventTime
-                                        + " Count : " + minuteSummary.f1
-                                );
-
-                                return minuteSummary;
+                            if(currentWatermark == 0L) {
+                                currentWatermark = currentTime;
+                            } else if(currentTime - currentWatermark > delay) {
+                                currentWatermark = currentTime;
                             }
+
+                            return new Watermark(currentWatermark - buffer);
+
                         }
-                );
 
-        // Collect late events for later processing
-        DataStream<Tuple2<String, Integer>> lateEvents =
-                finalTrail.getSideOutput(lateAuditTrail);
-
-        /*******************************************************************
-         * Process watermarked stream
-         *******************************************************************/
-        Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "localhost:9092");
-
-        // Standard flink kafka producer implementation
-        FlinkKafkaProducer<String> kafkaProducer = new FlinkKafkaProducer<String>(
-          "flink.kafka.streaming.sink",
-                (new KafkaSerializationSchema<String>() {
-                    @Override
-                    public ProducerRecord<byte[], byte[]> serialize(String s, @Nullable Long aLong) {
-                        return (new ProducerRecord<byte[], byte[]>(
-                                "flink.kafka.streaming.sink",
-                                s.getBytes()
-                        ));
+                        @Override
+                        public long extractTimestamp(AuditTrail auditTrail, long previousTimestamp) {
+                            return auditTrail.getTimestamp();
+                        }
                     }
-                }),
-                properties,
-                FlinkKafkaProducer.Semantic.EXACTLY_ONCE
-        );
+            );
 
-        finalTrail.map(
-                new MapFunction<Tuple2<String, Integer>, String>() {
-                    @Override
-                    public String map(Tuple2<String, Integer> finalTrail) throws Exception {
-                        return finalTrail.f0 + " = " + finalTrail.f1;
+            /*******************************************************************
+             * Process watermarked stream
+             *******************************************************************/
+
+            // Create output tag for late records
+            final OutputTag<Tuple2<String, Integer>> lateAuditTrail =
+                    new OutputTag<Tuple2<String, Integer>>("late-audit-trail"){};
+
+            // Use single output stream operator to produce side output
+            SingleOutputStreamOperator<Tuple2<String, Integer>> finalTrail =
+                    auditMarked
+                            .map(i -> new Tuple2<String, Integer>
+                                    (String.valueOf(i.getTimestamp()), 1)
+                            )
+                            .returns(Types.TUPLE(Types.STRING, Types.INT))
+                            .timeWindowAll(Time.seconds(1))
+                            .sideOutputLateData(lateAuditTrail)
+                            .reduce((x, y) ->
+                                    (new Tuple2<String, Integer>(x.f0, x.f1 + y.f1))
+                            )
+                            .map(
+                                    new MapFunction<Tuple2<String, Integer>, Tuple2<String, Integer>>() {
+                                        @Override
+                                        public Tuple2<String, Integer> map(Tuple2<String, Integer> minuteSummary) throws Exception {
+                                            String currentTime = (new Date()).toString();
+                                            String eventTime =
+                                                    (new Date(Long.valueOf(minuteSummary.f0))).toString();
+
+                                            System.out.println("Summary:"
+                                                    + " Current Time: " + currentTime
+                                                    + " Event Time: " + eventTime
+                                                    + " Count : " + minuteSummary.f1
+                                            );
+
+                                            return minuteSummary;
+                                        }
+                                    }
+                            );
+
+            // Collect late events for later processing
+            DataStream<Tuple2<String, Integer>> lateEvents =
+                    finalTrail.getSideOutput(lateAuditTrail);
+
+            /*******************************************************************
+             * Process watermarked stream
+             *******************************************************************/
+            Properties properties = new Properties();
+            properties.setProperty("bootstrap.servers", "localhost:9092");
+
+            // Standard flink kafka producer implementation
+            FlinkKafkaProducer<String> kafkaProducer = new FlinkKafkaProducer<String>(
+                    "flink.kafka.streaming.sink",
+                    (new KafkaSerializationSchema<String>() {
+                        @Override
+                        public ProducerRecord<byte[], byte[]> serialize(String s, @Nullable Long aLong) {
+                            return (new ProducerRecord<byte[], byte[]>(
+                                    "flink.kafka.streaming.sink",
+                                    s.getBytes()
+                            ));
+                        }
+                    }),
+                    properties,
+                    FlinkKafkaProducer.Semantic.EXACTLY_ONCE
+            );
+
+            finalTrail.map(
+                    new MapFunction<Tuple2<String, Integer>, String>() {
+                        @Override
+                        public String map(Tuple2<String, Integer> finalTrail) throws Exception {
+                            return finalTrail.f0 + " = " + finalTrail.f1;
+                        }
                     }
-                }
-        )
-        .addSink(kafkaProducer);
+            )
+                    .addSink(kafkaProducer);
 
-        /*******************************************************************
-         * Set up data source and execute pipeline
-         *******************************************************************/
-        Utils.printHeader("Starting file data generator");
-        Thread fileThread = new Thread(new FileStreamDataGenerator());
-        fileThread.start();
+            /*******************************************************************
+             * Set up data source and execute pipeline
+             *******************************************************************/
+            Utils.printHeader("Starting file data generator");
+            Thread fileThread = new Thread(new FileStreamDataGenerator());
+            fileThread.start();
 
-        streamEnv.execute("Flink watermarked stream example");
-
+            streamEnv.execute("Flink watermarked stream example");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
