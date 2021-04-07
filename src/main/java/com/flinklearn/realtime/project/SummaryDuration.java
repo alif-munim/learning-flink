08e3b5631ff800fb9d7c27ec8849df401fb3015e
Alif Munim <alif.munim@ryerson.ca>
2021-04-07 15:45:30 -0400

    Implement 10-second summaries

--- src/main/java/com/flinklearn/realtime/project/SummaryDuration.java
+++ src/main/java/com/flinklearn/realtime/project/SummaryDuration.java
@@ -5,13 +5,22 @@ import com.flinklearn.realtime.datasource.BrowserStreamDataGenerator;
 import com.flinklearn.realtime.datastreamapi.BrowserEvent;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks;
 import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.util.OutputTag;
 
 import javax.annotation.Nullable;
+import java.util.Date;
 import java.util.Properties;
 
 /****************************************************************************
@@ -31,6 +40,7 @@ public class SummaryDuration {
              ****************************************************************************/
             final StreamExecutionEnvironment streamEnv =
                     StreamExecutionEnvironment.getExecutionEnvironment();
+            streamEnv.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
             streamEnv.setParallelism(1);
 
             /****************************************************************************
@@ -96,6 +106,51 @@ public class SummaryDuration {
                                 }
                             }
                     );
+
+            // Create output tag for late records
+            final OutputTag<Tuple4<String, String, Long, Integer>> lateBrowserTrail =
+                    new OutputTag<Tuple4<String, String, Long, Integer>>("late-events"){};
+
+            // Process the watermarked stream
+            SingleOutputStreamOperator<Tuple4<String, String, Long, Integer>> browserEventProcessed = browserEventWatermarked
+                    .map(i -> new Tuple4<String, String, Long, Integer>(
+                            i.getUser(),
+                            i.getAction(),
+                            i.getTimestamp(),
+                            1
+                    ))
+                    .returns(Types.TUPLE(Types.STRING, Types.STRING, Types.LONG, Types.INT))
+                    .timeWindowAll(Time.seconds(1))
+                    .sideOutputLateData(lateBrowserTrail)
+                    .reduce((x, y) ->
+                        new Tuple4<String, String, Long, Integer>(
+                                x.f0,
+                                x.f1,
+                                x.f2,
+                                x.f3 + y.f3
+                        )
+                    )
+                    .map(new MapFunction<Tuple4<String, String, Long, Integer>, Tuple4<String, String, Long, Integer>>() {
+                        @Override
+                        public Tuple4<String, String, Long, Integer> map(Tuple4<String, String, Long, Integer> minuteSummary) throws Exception {
+                            String currentTime = (new Date()).toString();
+                            String eventTime =
+                                    (new Date(Long.valueOf(minuteSummary.f2))).toString();
+
+                            System.out.println("Summary:"
+                                    + " Current Time: " + currentTime
+                                    + ", User: " + minuteSummary.f0
+                                    + ", Action: " + minuteSummary.f1
+                                    + ", Event Time: " + eventTime
+                                    + ", Count : " + minuteSummary.f3
+                            );
+
+                            return minuteSummary;
+                        }
+                    });
+
+            DataStream<Tuple4<String, String, Long, Integer>> lateEvents =
+                    browserEventProcessed.getSideOutput(lateBrowserTrail);
 
             /****************************************************************************
              * Perform stateful operations to print event durations
